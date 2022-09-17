66cedfb60d49f2e19b783dce86dfd381508f54e9
rdavid87 <dlanordavid.1987@gmail.com>
2022-09-17 10:56:59 -0500

    Actualización archivos config

--- movie-service/src/main/java/com/dh/movieservice/MovieServiceApplication.java
+++ movie-service/src/main/java/com/dh/movieservice/MovieServiceApplication.java
@@ -2,6 +2,9 @@ package com.dh.movieservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 public class MovieServiceApplication {
