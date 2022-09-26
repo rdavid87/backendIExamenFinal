cc596366e7198efe5947b96acfa56f2d309d19bc
rdavid87 <dlanordavid.1987@gmail.com>
2022-09-26 08:44:27 -0500

    See actualiza SerieRepository y MovieRepository

--- catalog-service/src/main/java/com/dh/catalogservice/api/repository/MovieRepository.java
+++ catalog-service/src/main/java/com/dh/catalogservice/api/repository/MovieRepository.java
@@ -3,5 +3,9 @@ package com.dh.catalogservice.api.repository;
 import com.dh.catalogservice.api.model.movie.Movie;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface MovieRepository extends MongoRepository<Movie, String> {
+
+    List<Movie> findByGenre(String genre);
 }
--- catalog-service/src/main/java/com/dh/catalogservice/api/repository/SerieRepository.java
+++ catalog-service/src/main/java/com/dh/catalogservice/api/repository/SerieRepository.java
@@ -3,6 +3,9 @@ package com.dh.catalogservice.api.repository;
 import com.dh.catalogservice.api.model.serie.Serie;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface SerieRepository extends MongoRepository<Serie, String> {
 
+    List<Serie> findByGenre(String genre);
 }
